9486d3ee6beeef2c16bdd32728b1bb399f8f9324
kjsiason <95311740+kjsiason@users.noreply.github.com>
2023-05-19 21:40:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,35 @@
-#Library imports
+from __future__ import division
 import numpy as np
 import streamlit as st
 import cv2
 from keras.models import load_model
-#import joblib
 from keras.preprocessing import image
-from io import BytesIO
 from tensorflow.keras.applications import resnet50
-
+from io import BytesIO
 
 
 #Loading the Model
 model = load_model('classify.h5', compile=False)
-
-st.markdown("# Image Classifier")
-#Name of Classes
-
-
-#Uploading the dog image
-object_image = st.file_uploader("## Please upload an image", type=['png','jpg','webp','jpeg'])
+st.write("""
+# Image Classifier"""
+)
+#Uploading Image
+upload_image = st.file_uploader("Upload an image from your computer", type=['png','jpg','jpeg','webp'])
 submit = st.button('Classify Image')
-#On predict button click
 if submit:
 
-    if object_image is not None:
-
+    if upload_image is not None:
         # Convert the file to an opencv image.
-        file_bytes = np.asarray(bytearray(object_image.read()), dtype=np.uint8)
+        file_bytes = np.asarray(bytearray(upload_image.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
-        st.image(opencv_image, channels="BGR")
+        st.image(opencv_image, channels="BGR", caption="Uploaded Image", use_column_width=True)
         opencv_image = cv2.resize(opencv_image, (224,224))
         opencv_image.shape = (1,224,224,3)
-        predictions = model.predict(opencv_image)
-        predicted_classes = resnet50.decode_predictions(predictions, top=5)
-
-
-
+        pred = model.predict(opencv_image)
+        pred_classes = resnet50.decode_predictions(pred, top=4)
         # Displaying the image
-        #st.image(object_image, channels="BGR")
-        st.markdown("""This is an image of: """)
-
-        for imagenet_id, name, likelihood in predicted_classes[0]:
-            st.text('- {}: {:.2f} likelihood'.format(name,likelihood))
+        st.markdown("""
+        Results: """
+        )
+        for imagenet_id, name, likelihood in pred_classes[0]:
+            st.text('• {} with {:.0%} probability'.format(name,likelihood))
